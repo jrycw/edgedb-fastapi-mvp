4ed79beacb5fa59e1de3d26d1e01e245101b8720
jrycw <jerry@ycwu.space>
2024-02-22 02:54:06 +0800

    Revert "update test_health"
    This reverts commit 5b5aee1d859f71527dd68941030c68a56279a543.

--- tests/test_health.py
+++ tests/test_health.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import json
 from http import HTTPStatus
 
+import fastapi
 from fastapi.responses import JSONResponse
 
 
@@ -13,8 +14,9 @@ def test_healthy(mocker, health_url):
     expected_response = {
         "content": {"ok": ["edgedb.asyncio_client.AsyncIOClient"], "failing": []},
     }
-    mocked_get = mocker.patch(
-        "fastapi.testclient.TestClient.get",
+    mocked_get = mocker.patch.object(
+        fastapi.testclient.TestClient,
+        "get",
         return_value=JSONResponse(content=expected_response, status_code=HTTPStatus.OK),
     )
 
@@ -40,8 +42,9 @@ def test_not_healthy(mocker, health_url):
             ],
         },
     }
-    mocked_get = mocker.patch(
-        "fastapi.testclient.TestClient.get",
+    mocked_get = mocker.patch.object(
+        fastapi.testclient.TestClient,
+        "get",
         return_value=JSONResponse(
             content=expected_response, status_code=HTTPStatus.INTERNAL_SERVER_ERROR
         ),
