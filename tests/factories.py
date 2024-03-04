095dc355a755c347038540292f2e351ace3daa68
jrycw <jerry@ycwu.space>
2024-03-04 10:57:02 +0800

    modify tests/factories to reflect the real schema

--- tests/factories.py
+++ tests/factories.py
@@ -25,11 +25,24 @@ class TestUserDataWithnEvents(TestUserData):
 
 
 class TestEventData(TestAuditable):
+    """
+    The "address" and "schedule" fields are optional,
+    meaning they can be set to `None`, and adding randomness
+    may be beneficial for testing purposes.
+    """
+
     name: str = Field(default_factory=lambda: faker.text(max_nb_chars=20))
-    address: str = Field(default_factory=faker.street_address)
+    address: str = Field(
+        default_factory=random.choice([faker.street_address, lambda: None])
+    )
     schedule: str = Field(
-        default_factory=lambda: faker.date_time()
-        .replace(microsecond=random.randint(0, 1000000))
-        .isoformat()
+        default_factory=random.choice(
+            [
+                lambda: faker.date_time()
+                .replace(microsecond=random.randint(0, 1000000))
+                .isoformat(),
+                lambda: None,
+            ]
+        )
     )
     host_name: str = Field(default_factory=faker.name, max_length=50)
